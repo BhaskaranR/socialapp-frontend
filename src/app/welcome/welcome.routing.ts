3a8730caf714a645444ea4cac0c3e2ab5a34ea8b
Bhaskaran <r.bhaskarann@gmail.com>
2018-04-06 22:43:09 -0400

    change input container to mat-form-field

--- src/app/welcome/welcome.routing.ts
+++ src/app/welcome/welcome.routing.ts
@@ -8,7 +8,7 @@ import { NotFoundComponent } from '../not-found/not-found.component';
 
 const routes: Routes = [
     {
-        path: '',
+        path: 'login',
         component: WelcomeComponent,
         children: [
             {
