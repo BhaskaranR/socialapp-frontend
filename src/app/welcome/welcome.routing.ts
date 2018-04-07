37fcba51c20327dadda3edf95f177912e53af14d
Bhaskaran <r.bhaskarann@gmail.com>
2018-04-06 22:19:47 -0400

    fixes

--- src/app/welcome/welcome.routing.ts
+++ src/app/welcome/welcome.routing.ts
@@ -8,7 +8,7 @@ import { NotFoundComponent } from '../not-found/not-found.component';
 
 const routes: Routes = [
     {
-        path: 'login',
+        path: '',
         component: WelcomeComponent,
         children: [
             {
